21f6b842462c337325c2e8cd9e4e420f4f4f6aef
Ian <2842788037@qq.com>
2022-11-05 22:02:08 +0800

    good

--- segment.py
+++ segment.py
@@ -15,8 +15,8 @@ from predict import valid_seg
 # 超参数的设置
 parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
-parser.add_argument('--n_epochs', type=int, default=400, help='number of epochs of training')
-parser.add_argument('--batchSize', type=int, default=4, help='size of the batches')
+parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--batchSize', type=int, default=8, help='size of the batches')
 parser.add_argument('--dataroot', type=str, default='datasets/cyclegan/', help='root directory of the dataset')
 parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
@@ -44,7 +44,7 @@ criterion = CrossEntropyLoss(reduction='mean')
 
 # Optimizers & LR schedulers
 optimizer = torch.optim.Adam(params=segnet.parameters(), lr=opt.lr,betas=(0.9,0.99))
-lr_munet = torch.optim.lr_scheduler.StepLR(optimizer,100,gamma=0.2,last_epoch=-1)
+lr_munet = torch.optim.lr_scheduler.StepLR(optimizer,50,gamma=0.2,last_epoch=-1)
 
 # Data Transforms
 transforms_ = Transformation(opt).get()
